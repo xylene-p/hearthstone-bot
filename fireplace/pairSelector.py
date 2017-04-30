fab33bcd6772eb66e6aed8fac47d888841aa34a2
Chad <chad.hoang@yahoo.com>
2017-04-30 12:20:57 -0500

    [MISC] Removed heros from slection.[MISC] optimized pairing weights.

--- fireplace/pairSelector.py
+++ fireplace/pairSelector.py
@@ -15,17 +15,34 @@ class pairSelector:
 def GetOptimalDecisionPair(game: ".game.Game") -> ".game.Game":
 	allys = game.player1.characters
 
-	print("\n\n\n\n")
+	#	Remove the first index because it is the hero
+	allys.pop(0)
+
+	#	if list is empty return none
+	if len(allys) <= 0:
+		tempTup = (game.player1.characters[0], game.player1.characters[0].targets[0], 0)
+		return tempTup
+		#return None
+
+	print("\n\n\n --------------------------------------------	CHAD	--------------------------------------------\n")
 
 	#	list that holds the pairs for ally-to-enemy decision making
 	pairs = []
 
 	#	for each ally in the field...
 	for ally in allys:
-		#	if the ally is a enemy...
+
+		#if ally == allys[0]:
+		#	continue
 		#print("{}:\t{}\t\t{} HP".format(ally.controller, ally, ally.health))
+
+		enemies = ally.targets
+
+		#	Remove the first index because it is a hero
+		enemies.pop(0)
+
 		#	for each enemy the ally can enemy...
-		for enemy in ally.targets:
+		for enemy in enemies:
 			#	Calculate a weight for decision making between ally ally and it's enemy
 			decisionWeight = GetDecisionWeight(ally, enemy)
 
@@ -35,22 +52,23 @@ def GetOptimalDecisionPair(game: ".game.Game") -> ".game.Game":
 			pairs.append(AllyToEnemyWeight)
 			#print("\t\t{}".format(enemy))
 
-	#print(pairs)
+	for pair in pairs:
+		#if pair != None:
+			print("\033[1m[SOLUTION]\033[21m [WEIGHT: \033[32m{}\033[0m]\t{} with \033[33m{} ATK\033[0m and \033[31m{} HP\033[0m ATTACKS {} with \033[33m{} ATK\033[0m and \033[31m{} HP\033[0m.".format(pair[2], pair[0], pair[0].atk, pair[0].health, pair[1], pair[1].atk, pair[1].health))
 
 	#	Return a tuple that contains the pair with the highest decision weight
 	decisionPair = pairs[0]
-
 	for pair in pairs:
 		if pair[2] >= decisionPair[2]:
 			decisionPair = pair
 
-	print("{} with {} ATK and {} HP to attack {} with {} ATK and {} HP is a optimal choice given current events.".format(decisionPair[0], decisionPair[0].atk, decisionPair[0].health, decisionPair[1], decisionPair[1].atk, decisionPair[1].health))
+	print("[OPTIMAL SOLUTION] {} with {} ATK and {} HP to attack {} with {} ATK and {} HP.".format(decisionPair[0], decisionPair[0].atk, decisionPair[0].health, decisionPair[1], decisionPair[1].atk, decisionPair[1].health))
 	if decisionPair[2] <= 0:
-		print("Action has a weight of {}, better to not attack.".format(decisionPair[2]))
+		print("Action has a weight of {}, better to NOT attack.".format(decisionPair[2]))
 	else:
 		print("Action has a weight of {}".format(decisionPair[2]))
 
-	print("\n\n\n\n")
+	print("\n--------------------------------------------	CHAD	--------------------------------------------\n\n\n")
 
 	return decisionPair
 
@@ -68,8 +86,13 @@ def GetDecisionWeight(ally, enemy):
 	elif enemy.atk >= 0 and ally.atk <= 0:
 		weight += -9000
 
-	if ally.atk >= enemy.health:
+	#if ally.atk >= enemy.health:
+	if ally.atk <= enemy.health:
 		weight += ally.atk - enemy.health
+	else:
+		weight -= ally.atk - enemy.health
+
+	weight -= enemy.atk - ally.health
 
 	#if enemy.atk >= ally.health:
 	#	weight += -(enemy.atk + ally.health)
